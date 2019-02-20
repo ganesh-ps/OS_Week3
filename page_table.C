22b7972d72d461fbcf995374270680d5d427bd6c
sath-ganp <sath_ganp@tamu.edu>
2019-02-20 06:33:38 -0600

    Used KJs implementation
    2. Read K.J.’s tutorial on “Implementing Basic Paging”
    (http://www.osdever.net/tutorials/view/implementing-basic-paging) to understand
    how to set up basic paging.

--- page_table.C
+++ page_table.C
@@ -16,14 +16,41 @@ void PageTable::init_paging(ContFramePool * _kernel_mem_pool,
                             ContFramePool * _process_mem_pool,
                             const unsigned long _shared_size)
 {
-   assert(false);
-   Console::puts("Initialized Paging System\n");
+   //assert(false);
+   kernel_mem_pool = _kernel_mem_pool;
+   process_mem_pool = _process_mem_pool;
+   shared_size = _shared_size;
+   Console:R:puts("Initialized Paging System\n");
 }
 
 PageTable::PageTable()
 {
-   assert(false);
+   //assert(false);
    Console::puts("Constructed Page Table object\n");
+   *page_directory = (unsigned long *) 0x9C000;
+   unsigned long *page_table = (unsigned long *) 0x9D000; // the page table comes right after the page directory
+   unsigned long address=0; // holds the physical address of where a page is
+   unsigned int i;
+   // map the first 4MB of memory
+   for(i=0; i<1024; i++)
+   {
+     page_table[i] = address | 3; // attribute set to: supervisor level, read/write, present(011 in binary)
+     address = address + 4096; // 4096 = 4kb
+   };
+  // fill the first entry of the page directory
+  page_directory[0] = page_table; // attribute set to: supervisor level, read/write, present(011 in binary)
+  page_directory[0] = page_directory[0] | 3;
+  for(i=1; i<1024; i++)
+  {
+    page_directory[i] = 0 | 2; // attribute set to: supervisor level, read/write, not present(010 in binary)
+  };
+  /*
+  // write_cr3, read_cr3, write_cr0, and read_cr0 all come from the assembly functions
+     write_cr3(page_directory); // put that page directory address into CR3
+     write_cr0(read_cr0() | 0x80000000); // set the paging bit in CR0 to 1
+
+  // go celebrate or something 'cause PAGING IS ENABLED!!!!!!!!!!
+  */
 }
 
 
